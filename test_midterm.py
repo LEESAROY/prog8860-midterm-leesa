b94591111a8ed162ca47b7338d52057559b4762f
LEESAROY <42925369+LEESAROY@users.noreply.github.com>
2024-10-21 15:57:56 -0400

    created the dockerfile, test file, yml file and created the github secrets to use in yml

---
+++ test_midterm.py
@@ -0,0 +1,12 @@
+import unittest
+from midterm import app
+
+class TestMidterm(unittest.TestCase):
+    def test_home(self):
+        tester = app.test_client(self)
+        response = tester.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, b'This is Midterm exam!')
+
+if __name__ == '__main__':
+    unittest.main()
